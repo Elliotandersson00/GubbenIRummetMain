22bdb1c5c48e095f9bdddcdc5cb5ae2fd012bfaf
ifli87 <90029938+ifli87@users.noreply.github.com>
2021-09-20 09:46:33 +0200

    gubbubbe

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GubbenIRummet
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("gubbubbe");
         }
     }
 }
