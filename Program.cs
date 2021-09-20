df862375ec3955654c2f41f61a266409a0b83371
ifli87 <90029938+ifli87@users.noreply.github.com>
2021-09-20 09:44:49 +0200

    hej

--- Program.cs
+++ Program.cs
@@ -2,9 +2,9 @@
 
 namespace GubbenIRummet
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
