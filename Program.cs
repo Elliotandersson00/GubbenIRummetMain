d95a4ca14b94b97dc4c6c63391177934a70324c9
emil <emil.tornqvist@iths.se>
2021-09-23 15:10:44 +0200

    med console.clear();

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace GubbenIRummet
             GameTitle();
             Game currentGame = new Game();
             currentGame.Start();
-
+            Console.Clear();
             int answer;
             bool gameOver = false;
             while (!gameOver)
@@ -45,7 +45,7 @@ namespace GubbenIRummet
                 Console.ReadLine();
                 Console.ResetColor();
                 currentGame.Start();
-
+                Console.Clear();
                 Question question2 = new Question("     I datatyperna Float och Double kan du lagra??\n 1 = Endast heltal, Double kan lagra dubbelt så stora heltal som float\n 2 = Decimaltal\n 3 = Teckensträngar, Double kan lagra två strängar", 2);
                 Console.WriteLine(question2.Questions);
                 if (int.TryParse(Console.ReadLine(), out answer))
@@ -70,6 +70,7 @@ namespace GubbenIRummet
                 Console.ReadLine();
                 Console.ResetColor();
                 currentGame.Start();
+                Console.Clear();
                 Question question3 = new Question("     Hur många binära siffror innehåller en klassisk IP-adress??\n 1 = 16\n 2 = 64\n 3 = 32", 3);
                 Console.WriteLine(question3.Questions);
                 if (int.TryParse(Console.ReadLine(), out answer))
@@ -93,6 +94,7 @@ namespace GubbenIRummet
                 Console.ReadLine();
                 Console.ResetColor();
                 currentGame.Start();
+                Console.Clear();
                 Question question4 = new Question("     Vad är källkod?\n 1 = Kod till källardörren\n 2 = Den kod du skriver i t.ex Visual studio\n 3 = Ett slags hemligt tecken som endast de 'riktiga' programmerarna känner till. Tidigare arbetade programmerare ofta på källarvåningen därav uttrycket källkod", 2);
                 Console.WriteLine(question4.Questions);
                 if (int.TryParse(Console.ReadLine(), out answer))
@@ -117,6 +119,7 @@ namespace GubbenIRummet
                 Console.ReadLine();
                 Console.ResetColor();
                 currentGame.Start();
+                Console.Clear();
                 Question question5 = new Question("     Vad menas med kompilering?\n 1 = Omvandling från ett programspråk till maskinkod\n 2 = Ett sätt att komprimera program\n 3 = När du ger en variabel ett nytt värde görs en kompilering", 1);
                 Console.WriteLine(question5.Questions);
                 if (int.TryParse(Console.ReadLine(), out answer))
