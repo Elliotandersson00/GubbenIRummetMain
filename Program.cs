ef5217d1bfb8c5ee56924f682ec745a9c22942a7
Elliot Andersson <elliotandersson00@hotmail.com>
2021-09-23 13:49:29 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -8,13 +8,14 @@ namespace GubbenIRummet
 {
     internal class Program
     {
+        public static int answersFalse = 0;
         public static void Main(string[] args)
         {
             Game currentGame = new Game();
             currentGame.Start();
 
             
-            int answersFalse = 0;
+            
             int answer;
             bool gameOver = false;
             GameTitle();
@@ -39,8 +40,10 @@ namespace GubbenIRummet
                 }
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Rätt! Nästa fråga:");
+                Console.WriteLine("Rätt! Tryck enter för att fortsätta...");
+                Console.ReadLine();
                 Console.ResetColor();
+                currentGame.Start();
 
                 Question question2 = new Question("     I datatyperna Float och Double kan du lagra??\n 1 = Endast heltal, Double kan lagra dubbelt så stora heltal som float\n 2 = Decimaltal\n 3 = Teckensträngar, Double kan lagra två strängar", 2);
                 Console.WriteLine(question2.Questions);
@@ -61,15 +64,19 @@ namespace GubbenIRummet
                 }
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Rätt! Nästa fråga:");
+                Console.WriteLine("Rätt! Tryck enter för att fortsätta...");
+                Console.ReadLine();
                 Console.ResetColor();
+                currentGame.Start();
                 Question question3 = new Question("     Hur många binära siffror innehåller en klassisk IP-adress??\n 1 = 16\n 2 = 64\n 3 = 32", 3);
                 Console.WriteLine(question3.Questions);
                 if (int.TryParse(Console.ReadLine(), out answer))
                 {
                     if (question3.CheckAnswer(answer) != true)
                     {
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Fel svar, försök igen!");
+                        Console.ResetColor();
                         answersFalse++;
                         continue;
                     }
@@ -80,8 +87,10 @@ namespace GubbenIRummet
                     continue;
                 }
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Rätt! Nästa fråga:");
+                Console.WriteLine("Rätt! Tryck enter för att fortsätta...");
+                Console.ReadLine();
                 Console.ResetColor();
+                currentGame.Start();
                 Question question4 = new Question("     Vad är källkod?\n 1 = Kod till källardörren\n 2 = Den kod du skriver i t.ex Visual studio\n 3 = Ett slags hemligt tecken som endast de 'riktiga' programmerarna känner till. Tidigare arbetade programmerare ofta på källarvåningen därav uttrycket källkod", 2);
                 Console.WriteLine(question4.Questions);
                 if (int.TryParse(Console.ReadLine(), out answer))
@@ -102,8 +111,10 @@ namespace GubbenIRummet
                 }
 
                 Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Rätt! Nästa fråga:");
+                Console.WriteLine("Rätt! Tryck enter för att fortsätta...");
+                Console.ReadLine();
                 Console.ResetColor();
+                currentGame.Start();
                 Question question5 = new Question("     Vad menas med kompilering?\n 1 = Omvandling från ett programspråk till maskinkod\n 2 = Ett sätt att komprimera program\n 3 = När du ger en variabel ett nytt värde görs en kompilering", 1);
                 Console.WriteLine(question5.Questions);
                 if (int.TryParse(Console.ReadLine(), out answer))
@@ -124,28 +135,43 @@ namespace GubbenIRummet
                 }
 
                 // Lägg in fler frågor här.
+                CheckVictory();
+                gameOver = true;
+            }
 
+
+
+            static void CheckVictory()
+            {
+                SoundPlayer victoryNoise = new SoundPlayer("applause_y.wav");
                 if (answersFalse < 1)
                 {
                     Console.ForegroundColor = ConsoleColor.Blue;
 
-                    SoundPlayer victoryNoise = new SoundPlayer("applause_y.wav");
+                   
                     victoryNoise.Load();
                     victoryNoise.Play();
 
                     Console.WriteLine("     Grattis! Du svarade inte fel någon gång!");
                     Console.ReadLine();
                 }
-                else
+                else if(answersFalse == 1)
                 {
-                    Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.ForegroundColor = ConsoleColor.Blue;
 
-                    SoundPlayer victoryNoise = new SoundPlayer("applause_y.wav");
-                    victoryNoise.Load();
-                    victoryNoise.Play();
+                        victoryNoise.Load();
+                        victoryNoise.Play();
+                        Console.WriteLine($"     Grattis! Du svarade bara fel {answersFalse} gång!");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
 
-                    Console.WriteLine($"     Grattis! Du svarade bara fel {answersFalse} gånger!");
-                    Console.ReadLine();
+                        victoryNoise.Load();
+                        victoryNoise.Play();
+                        Console.WriteLine($"     Grattis! Du svarade bara fel {answersFalse} gånger!");
+                        Console.ReadLine();
+                    }
                 }
             }
 
@@ -171,4 +197,3 @@ namespace GubbenIRummet
             }
         }
     }
-}
